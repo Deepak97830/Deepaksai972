b9fe7cee93cd23f103ee791e72df8637546cd4ce
Deepak97830 <saideepakvsmsp@gmail.com>
2024-03-17 23:02:06 +0530

    add pages

---
+++ code1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+     
+    <div className="App">
+    
+        welcome thank you
+    </div>
+  );
+}
+
+export default App;
